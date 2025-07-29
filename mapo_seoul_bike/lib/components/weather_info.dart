daed402ec4dd1a32cee40792db5888be92695163
whatslung <200623984+whatslung@users.noreply.github.com>
2025-07-29 18:41:32 +0900

    웹 개발 시작 파일

---
+++ mapo_seoul_bike/lib/components/weather_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherInfo extends StatelessWidget {
+  const WeatherInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  
+      Container( 
+          width: 300,
+              height: 300,
+              color: Colors.white,
+            );
+  }
+}
---
+++ mapo_seoul_bike/lib/view/home.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:mapo_seoul_bike/components/appbar_title.dart';
+import 'package:mapo_seoul_bike/components/main_map.dart';
+import 'package:mapo_seoul_bike/components/station_info.dart';
+import 'package:mapo_seoul_bike/components/weather_info.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80), // 원하는 높이 지정 (ex: 80)
+        child: AppBar(
+          title: Appbartitle(),
+          actions: [
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: SizedBox(
+                  width: 300,
+                  height: 45,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: '대여소 이름을 입력하세요',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                //
+              },
+              icon: Icon(Icons.search_rounded),
+            ),
+          ], 
+          backgroundColor: Colors.white,
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            color: Color(0xff3D5567),
+            
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+                color: Color(0xff30E291)
+              ),
+              height: MediaQuery.of(context).size.height * 0.7,
+              
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: ListView(
+              children: [
+                ResponsiveRowColumn(
+                  rowMainAxisAlignment: MainAxisAlignment.center,
+                  rowPadding: EdgeInsets.all(30),
+                  columnPadding: EdgeInsets.all(30),
+                  layout: ResponsiveBreakpoints.of(context).isDesktop
+                      ? ResponsiveRowColumnType.ROW
+                      : ResponsiveRowColumnType.COLUMN,
+                  children: [
+                    ResponsiveRowColumnItem(
+                      rowFlex: 1,
+                      child:
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 40),
+                            child: WeatherInfo(),
+                          ), //model에서 가져올 정보있으면 가져오기 예 (child: CourseTile(course: courses[0]))
+                    ),
+                    ResponsiveRowColumnItem(
+                      rowFlex: 1, 
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 40),
+                        child: StationInfo(),
+                      )
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Center(child: MainMap()),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  } // build 닫힘
+
+  //---functions---
+} // clas
+
+//반응형 필요시 사용
+// ResponsiveVisibility(
+//     hiddenConditions: [
+//       Condition.largerThan(value: true, name: MOBILE) //mobile보다 크면 보여준다.
+//     ],
+//     child: MenuTextButton(text: 'Course'),
+//   ),
