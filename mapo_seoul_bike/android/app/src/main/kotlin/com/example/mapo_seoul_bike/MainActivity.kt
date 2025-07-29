603f8b097be20735bb470d615be7af0a1d5fa7f7
whatslung <200623984+whatslung@users.noreply.github.com>
2025-07-29 18:41:32 +0900

    웹 개발 시작 파일

---
+++ mapo_seoul_bike/android/app/src/main/kotlin/com/example/mapo_seoul_bike/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mapo_seoul_bike
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mapo_seoul_bike/lib/components/main_map.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainMap extends StatelessWidget {
+  const MainMap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+              width: 800,
+              height: 400,
+              color: Colors.white,
+    );
+  }
+}
